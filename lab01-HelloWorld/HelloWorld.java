9f4e836ab4cf79d0ea6793e3333b5a774c92c7e7
jic518 <jic518@lehigh.edu>
2014-08-29 14:45:34 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,29 @@
+////////////////////////////////////////////////
+//Ji Ho Choi
+//CSE002
+//Lab01-HelloWorld
+
+//First compile the program
+//      javac HelloWorld.java
+//      java HelloWorld//
+
+//  define a class
+public class HelloWorld{
+    
+// add main method
+    public static void main(String[] args) {
+        
+// print the statement with box around
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("#                      #");
+        System.out.println("#  Hello World 2014!!  #");
+        System.out.println("#                      #");
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("test");
+        
+        //test/testtest
+    }
+}
+        
