22bff34bd0fd707e466123011c472d496f1f8b57
jic518 <jic518@lehigh.edu>
2014-08-30 15:34:08 +0000

    updated hw01(WelcomeClass)

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,31 @@
+/*Ji Ho Choi
+8/30/14
+Fundamentals of Programming
+*/
+
+////define the class
+public class WelcomeClass{
+
+/////Add main method(1tab)
+    public static void main (String[] args) {
+    
+//Print the statement (2tabs) important to put semicolon
+        System.out.println("  -----------");
+        System.out.println("  | WELCOME |");
+        System.out.println("  -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("|-j--i--c--5--1--8-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("I am a student from South Korea");
+        System.out.println("I like playing games and soccer.");
+////////Got the error message illegal escape Character. What does that mean?
+////////seems like backslash is a special character in java needs extra backslash
+//after backslash. / \\/ \\/ \\/ \\/ \\/ \\ changed input
+
+        
+    }
+}
+        
+        
