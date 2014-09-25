a384f31112f9ae3888735bea2f691d402c056aab
jic518 <jic518@lehigh.edu>
2014-09-25 02:57:40 +0000

    updated hw04

--- hw04/TimePadding.java
+++ hw04/TimePadding.java
@@ -5,32 +5,26 @@ import java.util.Scanner;
 public class TimePadding {
     public static void main(String[] args) {
         Scanner myScanner;
-        int minutes, hours;
+        
         myScanner = new Scanner(System.in);
         System.out.print("Enter the time in seconds- ");
         if (myScanner.hasNextInt()) { //if 1 (verifies if it is an integer)
             int Seconds = myScanner.nextInt();
-                    if (Seconds < 60 && Seconds%60>=10) {//if2 (if it is more than 60 seconds, it will calculate it into minutes)
-                System.out.println("The time is "+"00:00:"+Seconds%60);
-                
-            }//if2
-            if (Seconds%60<10 && Seconds < 60) {//this if will add 0 in front of the seconds if the number turns out to be a single number
-                    System.out.println("The time is "+"00:00:0"+Seconds%60);
-                }// end of if 0 turns out single second
-            else if (Seconds >= 60 && ((Seconds/60)-((Seconds/60)%1)) > 10 &&Seconds <= 3600) {//else if 1
-                System.out.println("The time is "+"00:"+((Seconds/60)-((Seconds/60)%1))+":"+Seconds%60);
-                 if (Seconds%60<10 && Seconds < 60) {//this if will add 0 in front of the seconds if the number turns out to be a single number
-                    System.out.println("The time is "+"00:00:0"+Seconds%60);
-                }// I need to copy this in case I will have eg. 65 secs
-            }//end of else if 1
-            else if (Seconds >= 60 && ((Seconds/60)-((Seconds/60)%1)) < 10 &&Seconds <= 3600) {
-                System.out.println("The time is "+"00:0"+((Seconds/60)-((Seconds/60)%1))+":"+Seconds%60);
-                 if (Seconds%60<10 && Seconds < 60) {//this if will add 0 in front of the seconds if the number turns out to be a single number
-                    System.out.println("The time is "+"00:00:0"+Seconds%60);
-                }// end of if 0 turns out single second
+           
+            int hours = (Seconds/3600);
+            int minutes = (Seconds/60)-(Seconds/60)%1-(hours*60);
+        
+            if (minutes < 10) {
+                System.out.print("The time is "+hours+":0" + minutes);
             }
-            else if (Seconds >=3600) {
-                System.out.println("The time is "+((Seconds/3600)-((Seconds/3600)%1))+":0"+((Seconds/3600)-(Seconds/3600)%60)+":0"+Seconds%60);
+            else if (minutes > 10) {
+                System.out.print("The time is "+hours+":"+minutes);
+            }
+            if  ((Seconds-(3600*hours)-(60*minutes-minutes%1)) < 10) {
+                System.out.println(":"+"0"+((Seconds-(3600*hours)-(60*minutes-minutes%1))));
+            }
+            else if ((Seconds-(3600*hours)-(60*minutes-minutes%1)) > 10) {
+                System.out.println(":"+((Seconds-(3600*hours)-(60*minutes-minutes%1))));
             }
             
             
--- just practicing!/howdoIput0.java
+++ just practicing!/howdoIput0.java
@@ -1,11 +1,8 @@
 public class howdoIput0 {
     public static void main(String[] args) {
-        int h.parseInt(args[0]);  
-java.text.DecimalFormat nft = new  
-java.text.DecimalFormat("#00.###");  
-nft.setDecimalSeparatorAlwaysShown(false);  
-System.out.println(nft.format(i));  
-        
+   int java = 5.11111;
+   System.out.println(java-java%1);
+            
         
     }
 }
