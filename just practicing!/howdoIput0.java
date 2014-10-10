abd57082c63110717dfeb65219ff178244dd5bf0
jic518 <jic518@lehigh.edu>
2014-10-10 19:44:47 +0000

    updated RandomGames

--- just practicing!/howdoIput0.java
+++ just practicing!/howdoIput0.java
@@ -1,9 +1,8 @@
 public class howdoIput0 {
     public static void main(String[] args) {
         
-       int j;
-       j = 15;
-       System.out.println(j++);
+        int Roulette = (int)(1+Math.random() * 2);
+        System.out.println(Roulette);
    
     }
 }
--- lab05/RandomGames.java
+++ lab05/RandomGames.java
@@ -2,18 +2,32 @@ import java.util.Scanner;
 
 public class RandomGames {
    public static void main(String[] args) {
-       //First I need to generate a lottery number]
-       int Roulette = (int)(Math.random() * 36);
+       //First I need to generate a lottery number
+       int Roulette = (int)(1+Math.random() * 40);
        Scanner myScanner = new Scanner(System.in);
        System.out.print("Enter R or r for roulette, C or c for craps, P or p for pick a card: ");
-       if (myScanner.hasNextLine()) {
-           String EnteraGame = myScanner.nextLine();
-           if (EnteraGame == "R" || EnteraGame == "r") {
-               System.out.println("hja");
+       
+           String EnteraGame = myScanner.next();
+           int RResult=Roulette;
+           if (EnteraGame.equals("r") || EnteraGame.equals("R")) {
+               switch (Roulette) {
+                   case 1: System.out.println("00");
+                   break;
+                   case 2: System.out.println("0");
+                   break;
+                   default: System.out.println(RResult-4);
+                   break;
+               }//end of switch
            }//end of Roulette
-       }//if for has next string
-       else {
-           System.out.println("What are you saying");
-       }
+           else if (EnteraGame.equals("C") || EnteraGame.equals("c")) {
+               System.out.println("Craps not implemented yet");
+           }//end of craps
+           else if (EnteraGame.equals("P") || EnteraGame.equals("p")) {
+               System.out.println("Picking a card not implemented");
+           }
+           else {
+               System.out.println("'"+EnteraGame+"' is not one of 'R','r','C','c','P','p' ");
+           }
+     
    } //end of main method
 }//end of class
