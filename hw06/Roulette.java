6acb3dad2f35a1a895c422c463ced718f84ff9c6
jic518 <jic518@lehigh.edu>
2014-10-15 02:32:35 +0000

    updated Roulette

--- hw06/Roulette.java
+++ hw06/Roulette.java
@@ -4,7 +4,7 @@
 public class Roulette {
     public static void main(String[] args) {
         
-        int Bet = (int)(1+Math.random() * 38);
+       
         int SimHund = 0;
         int SimThousand = 0;
         int NumMatchesHun = 0;
@@ -15,28 +15,36 @@ public class Roulette {
         while (SimThousand < 1000) {//experiment while
         
         SimThousand++;
-        
+        int Bet = (int)(1+Math.random() * 38);
         while (SimHund < 100){
         SimHund++;
         int Roulette = (int)(1+Math.random() * 38);
             if (Roulette == Bet) {
             NumMatchesHun++;
+            NumMatchThou++;
             }//this is an if statement that will calculate how many times the roulette number matches the bet
             
-        }//this is a while statement that will simulate the Roulette for 1000 times
+        
+        
         if (NumMatchesHun >= 3) {
             NumoftimesProfit++;
+            NumMatchesHun = 0;
             }// if to calculate number of profit a user makes for a single 100 roulette roll
     
-        if (NumMatchesHun==0){
+        else if (NumMatchesHun==0){
             LossProfit++;
+            NumMatchesHun = 0;
         }
         
+        }//this is a while statement that will simulate the Roulette for 1000 times
+        
+       
+        SimHund = 0;
         }//experiment while
         
         System.out.println("I've got profit "+ NumoftimesProfit+" times");
         System.out.println("I've lost everything "+LossProfit+" times");
-        System.out.println("I've won "+ NumMatchesHun+" times");
+        System.out.println("I've won "+ NumMatchThou+" times");
         
         
         
