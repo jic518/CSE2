b9fc56a18cb5ac5efd1b17124e829b98e92a6c30
jic518 <jic518@lehigh.edu>
2014-10-24 02:51:32 +0000

    updated hw07

--- hw07/NumberStack.java
+++ hw07/NumberStack.java
@@ -11,20 +11,34 @@ public class NumberStack {
             if (myScanner.hasNextInt()) {
 
                 int Input = myScanner.nextInt();
+                int s = 1;
+                int No = Input - s;
                 if (Input >= 1 && Input <= 9) {
                     System.out.println("using for loops: ");
 
                     for (i = 1; i <= Input; i++) {
                         int increment = 2 * i - 1;
+
+
+
                         for (int c = 1; c <= i; c++) {
 
+                            for (s = Input; s > i; s--) {
+                                System.out.print(" ");
+
+                            }
                             for (int j = 1; j <= increment; j++) {
 
                                 System.out.print(i);
+
                             }
 
                             System.out.println("");
                         }
+                        for (s = Input; s > i; s--) {
+                                System.out.print(" ");
+
+                            }
                         for (int b = 1; b <= increment; b++) {
                             System.out.print("-");
                         }
@@ -39,72 +53,72 @@ public class NumberStack {
                     int e = 1;
                     int f = 1;
                     int g = 1;
-                    
+
                     while (d <= Input) {
                         int increment2 = 2 * d - 1;
                         while (e <= d) {
                             while (f <= increment2) {
                                 System.out.print(d);
                                 f++;
-    
+
                             }
-                            
-                            
+
+
                             System.out.println("");
                             e++;
-                            f=1;
+                            f = 1;
                         }
-                        e=1;
+                        e = 1;
                         while (g <= increment2) {
-                            
+
                             System.out.print("-");
                             g++;
                         }
-                        g=1;
+                        g = 1;
                         System.out.println("");
                         d++;
-            
+
                     }
-                    
+
                     ////////////Start of do while loops
-                    
+
                     System.out.println("Using do while loops: ");
                     int Last1 = 1;
                     int Last2 = 1;
                     int Last3 = 1;
                     int Last4 = 1;
-                    
+
                     do {
                         int increment3 = 2 * Last1 - 1;
                         do {
                             do {
                                 System.out.print(Last1);
                                 Last3++;
-    
+
                             }
                             while (Last3 <= increment3);
-                            
-                            
+
+
                             System.out.println("");
                             Last2++;
-                            Last3=1;
+                            Last3 = 1;
                         }
                         while (Last2 <= Last1);
-                        Last2=1;
+                        Last2 = 1;
                         do {
-                            
+
                             System.out.print("-");
                             Last4++;
                         }
                         while (Last4 <= increment3);
-                        Last4=1;
+                        Last4 = 1;
                         System.out.println("");
                         Last1++;
-            
+
                     }
                     while (Last1 <= Input);
-                    
-                    
+
+
                 } //end of if for the 1-9    
 
 
