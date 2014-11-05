61cb857242a042a3ac04f35cb62ffe30be6bd87f
jic518 <jic518@lehigh.edu>
2014-11-05 02:40:50 +0000

    updated comments for  hw09

--- hw09/BlockedAgain.java
+++ hw09/BlockedAgain.java
@@ -7,13 +7,13 @@ public class BlockedAgain {
             allBlocks(m);
         } //end of main method
     public static int getInt(){
-        Scanner myScanner;
+        Scanner myScanner;//call scanner
         myScanner = new Scanner(System.in);
-        boolean Check=false;
+        boolean Check=false;// this is a boolean to check if it is integer
         int User = -1;
         while(Check != true) {
             System.out.print("Enter an integer between 1 and 9, inclusive: ");
-            if(checkInt(myScanner)){
+            if(checkInt(myScanner)){//this will bring the checkInt method
             User = myScanner.nextInt();
                 if(checkRange(User)) {
                     Check = true;
@@ -33,7 +33,7 @@ public class BlockedAgain {
             myScanner.next();
             Check = false;
         }
-        return Check;
+        return Check;// this method will check the scanner and see if it is an integer
     }//end of checkInt
     public static boolean checkRange(int b){
         boolean Check;
@@ -50,10 +50,9 @@ public class BlockedAgain {
     int s = 1;
                 int No = x - s;
                 if (x >= 1 && x <= 9) {
-                    System.out.println("using for loops: ");
 
                     for (int i = 1; i <= x; i++) {
-                        int increment = 2 * i - 1;
+                        int increment = 2 * i - 1;//this calculates how many numbers there will be in a line
 
 
 
@@ -65,20 +64,20 @@ public class BlockedAgain {
                             }
                             for (int j = 1; j <= increment; j++) {
 
-                                System.out.print(i);
+                                System.out.print(i);//this will print out how many numbers
 
                             }
 
                             System.out.println("");
                         }
-                        for (s = x; s > i; s--) {
+                        for (s = x; s > i; s--) {// for loop for the indent
                                 System.out.print(" ");
 
                             }
                         for (int b = 1; b <= increment; b++) {
                             System.out.print("-");
                         }
-                        System.out.println("");
+                        System.out.println("");//space that will come after the system prints out -
                     }
                 }
     }//this method will be called to make the rocket
